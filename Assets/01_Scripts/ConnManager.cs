bc066b873f82f137ab48b08f14d565fe05c529d7
mjm0414 <mjm0414@Naver.com>
2024-03-28 00:37:19 +0900

    포톤 서버 설정 및 각종 설정

---
+++ Assets/01_Scripts/ConnManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class ConnManager : MonoBehaviourPunCallbacks
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
