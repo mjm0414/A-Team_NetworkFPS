0491d5a86777e871906d2219d9efdc9a80985c08
mjm0414 <mjm0414@Naver.com>
2024-03-28 01:54:26 +0900

    포톤 서버 설정

--- Assets/01_Scripts/ConnManager.cs
+++ Assets/01_Scripts/ConnManager.cs
@@ -8,7 +8,16 @@ public class ConnManager : MonoBehaviourPunCallbacks
 
     void Start()
     {
-        
+        PhotonNetwork.GameVersion = "1.0";      // 일단 게임 버젼은 1.0으로 한다 
+
+        int num = Random.Range(0, 1000);    // 일단 이름은 랜덤하게 뽑는다 
+
+        // 원래 이 단계에서 ID와 Password를 입력하는 단계
+        PhotonNetwork.NickName = num.ToString();    // 랜덤하게 뽑은 숫자를 닉네임으로 한다 
+
+        PhotonNetwork.AutomaticallySyncScene = true;    // 씬을 자동으로 동기화 시킨다 
+
+        PhotonNetwork.ConnectUsingSettings();   // 마스터 서버에 접속한다
     }
 
 
